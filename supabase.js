f684600091688de644d1d9d43f0a08a511fc4b9f
LIGONE Project <rene.herrchen@gmail.com>
2026-02-15 20:57:22 +0100

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
-const supabase = window.supabase.createClient(
+// Humans Together – Supabase Client (global)
+
+window.supabaseClient = window.supabase.createClient(
   "https://hwpeegcsdgxgcybuosfv.supabase.co",
   "sb_publishable_LhuHoUS_DR4o2Pp3aqbNBw_IXwHo2fD"
 );
-
-window.supabaseClient = supabase;
