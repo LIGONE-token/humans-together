2f0b6a2d43db68b47ac081bed7736bfc8d76cd74
LIGONE Project <rene.herrchen@gmail.com>
2026-02-15 20:25:09 +0100

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,6 @@
+const supabase = window.supabase.createClient(
+  "https://hwpeegcsdgxgcybuosfv.supabase.co",
+  "sb_publishable_LhuHoUS_DR4o2Pp3aqbNBw_IXwHo2fD"
+);
+
+window.supabaseClient = supabase;
