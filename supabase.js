f277a9c7f6826bd690133f68085877cd19bf532c
LIGONE Project <rene.herrchen@gmail.com>
2026-02-16 21:12:04 +0100

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,4 +1,11 @@
 window.sb = window.supabase.createClient(
   "https://hwpeegcsdgxgcybuosfv.supabase.co",
-  "sb_publishable_LhuHoUS_DR4o2Pp3aqbNBw_IXwHo2fD"
+  "sb_publishable_LhuHoUS_DR4o2Pp3aqbNBw_IXwHo2fD",
+  {
+    auth: {
+      persistSession: true,
+      autoRefreshToken: true,
+      detectSessionInUrl: true
+    }
+  }
 );
